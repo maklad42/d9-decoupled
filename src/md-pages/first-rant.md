4572342ef50394031da245fdd51e14e5a64d19a6
maklad42 <maklad42+git@gmail.com>
2021-06-18 21:54:02 +0900

    Add the collection route for the markdown files, and a first blog post. 210618

---
+++ src/md-pages/first-rant.md
@@ -0,0 +1,8 @@
+---
+slug: "/blog/2021-06-18"
+date: "2021-06-18"
+title: "Seth-inspired Rants"
+---
+
+This is my first rant. Inspired by the one and only Seth Godin.
+Check out his podcast at Akimbo.link.
---
+++ src/pages/{MarkdownRemark.frontmatter__slug}.js
@@ -0,0 +1,34 @@
+import React from "react"
+import { graphql } from "gatsby"
+
+export default function MarkDownTemplate({
+  data, // injected from the GraphQL query below
+}) {
+  const { markdownRemark } = data // data.markdownRemark holds the post data
+  const { frontmatter, html } = markdownRemark
+  return (
+    <div className="blog-post-container">
+      <div className="blog-post">
+        <h1>{frontmatter.title}</h1>
+        <h2>{frontmatter.data}</h2>
+        <div
+          className="blog-post-content"
+          dangerouslySetInnerHTML={{ __html: html }}
+        />
+      </div>
+    </div>
+  )
+}
+
+export const pageQuery = graphql`
+  query($id: String!) {
+    markdownRemark(id: { eq: $id }) {
+      html
+      frontmatter {
+        date(formatString: "MMMM DD, YYYY")
+        slug
+        title
+      }
+    }
+  }
+`
