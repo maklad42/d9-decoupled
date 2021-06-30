0c0c548d26ef1cba31cffc5b6999e64e58ce1c6e
maklad42 <maklad42+git@gmail.com>
2021-06-30 23:05:56 +0900

    Adds graphql query to the Gtd component. 210630

--- src/pages/gtd.js
+++ src/pages/gtd.js
@@ -6,3 +6,23 @@ import Layout from "../components/layout"
 const Gtd = () => <Layout>Some text.</Layout>
 
 export default Gtd
+
+export const query = graphql`
+  query {
+    allNodeTodo {
+      edges {
+        node {
+          title
+          field_start_date
+          field_due_by
+          relationships {
+            field_project {
+              field_project_active
+              name
+            }
+          }
+        }
+      }
+    }
+  }
+`
