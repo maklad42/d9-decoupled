cc00f43dd5055f8c8360f2399b49cb8b2f6239b0
maklad42 <maklad42+git@gmail.com>
2021-07-02 22:32:46 +0900

    Add mapping for graphlql query on the gtd page. 210702

--- src/pages/gtd.js
+++ src/pages/gtd.js
@@ -3,7 +3,18 @@ import { graphql } from "gatsby"
 
 import Layout from "../components/layout"
 
-const Gtd = () => <Layout>Some text.</Layout>
+const Gtd = ({ data }) => (
+  <Layout>
+    {data.allNodeTodo.edges.map(todo => (
+      <>
+        <div className="todo">{todo.node.relationships.field_project.name}</div>
+        <div className="todo">{todo.node.title}</div>
+        <div className="todo">Start: {todo.node.field_start_date}</div>
+        <div className="todo">Due: {todo.node.field_due_date}</div>
+      </>
+    ))}
+  </Layout>
+)
 
 export default Gtd
 
