2409c3883fb4751f6097bc07c5f0afe03dfb2d92
maklad42 <maklad42+git@gmail.com>
2021-07-03 22:57:56 +0900

    Makes the gtd page graphql query group results by the project. 210703

--- src/pages/gtd.js
+++ src/pages/gtd.js
@@ -5,12 +5,14 @@ import Layout from "../components/layout"
 
 const Gtd = ({ data }) => (
   <Layout>
-    {data.allNodeTodo.edges.map(todo => (
+    {data.allNodeTodo.group.map(grp => {
       <>
-        <div className="todo">{todo.node.relationships.field_project.name}</div>
-        <div className="todo">{todo.node.title}</div>
-        <div className="todo">Start: {todo.node.field_start_date}</div>
-        <div className="todo">Due: {todo.node.field_due_date}</div>
+        <div className="todo">
+          {grp.node[0].relationships.field_project.name}
+        </div>
+        <div className="todo">{grp.node.title}</div>
+        <div className="todo">Start: {grp.node.field_start_date}</div>
+        <div className="todo">Due: {grp.node.field_due_date}</div>
       </>
     ))}
   </Layout>
@@ -20,16 +22,18 @@ export default Gtd
 
 export const query = graphql`
   query {
-    allNodeTodo {
-      edges {
-        node {
-          title
-          field_start_date
-          field_due_by
-          relationships {
-            field_project {
-              field_project_active
-              name
+    allNodeTodo(sort: { fields: [field_due_by], order: ASC }) {
+      group(field: relationships___field_project___name) {
+        edges {
+          node {
+            title
+            field_start_date
+            field_due_by
+            relationships {
+              field_project {
+                field_project_active
+                name
+              }
             }
           }
         }
