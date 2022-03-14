c0fc8ba0165cbe258479f09b94074f94f87cd658
maklad42 <maklad42+git@gmail.com>
2022-03-14 22:31:05 +0900

    Adds schedule bar component to the Tasks Component, 220314

---
+++ src/components/days.js
@@ -0,0 +1,5 @@
+import React from "react"
+
+const Days = () => <td className="schedule">x</td>
+
+export default Days
--- src/components/tasks.js
+++ src/components/tasks.js
@@ -1,16 +1,31 @@
 import React from "react"
+import Days from "./days"
 
 const Tasks = ({ tasks }) => (
   <>
     {tasks.edges.map(task => (
       <>
-        <div className="task-wrapper">
+        <table className="project todolist">
+          <tbody>
+            <tr className="task-wrapper">
+              <td className="task title">{task.node.title}</td>
+              <td className="task due">{task.node.field_sub_project}</td>
+              <td className="task start">{task.node.field_start_date}</td>
+              <td className="task due">{task.node.field_due_by}</td>
+              <td className="task due">{task.node.field_done}</td>
+              {[...Array(10)].map((v, i) => (
+                <Days key={i} />
+              ))}
+            </tr>
+          </tbody>
+        </table>
+        {/* <div className="task-wrapper">
           <div className="task title">{task.node.title}</div>
           <div className="task due">{task.node.field_sub_project}</div>
           <div className="task start">{task.node.field_start_date}</div>
           <div className="task due">{task.node.field_due_by}</div>
           <div className="task due">{task.node.field_done}</div>
-        </div>
+        </div> */}
       </>
     ))}
   </>
